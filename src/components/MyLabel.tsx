14c026e8635fb58602c8930594d2e869e3cdf4ef
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2022-05-22 23:13:20 -0400

    added args, props to myLabel storybook

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,5 +1,31 @@
 import "./mylabel.css";
 
-export const MyLabel = () => {
-  return <span>Hola mundo</span>;
+export interface MyLabelProps {
+  /**
+   * This is the title Label
+   */
+  label: string;
+  /**
+   * This is sizes of the label
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Color Label
+   */
+  color: "primary" | "secondary" | "tertiary";
+  /**
+   * Uppercase to title Label
+   */
+  allCaps?: boolean;
+}
+
+export const MyLabel = ({
+  label = "No Label",
+  size = "normal",
+  color = "primary",
+  allCaps,
+}: MyLabelProps) => {
+  return (
+    <span className={`${size} text-${color}`}>{allCaps ? label.toUpperCase() : label}</span>
+  );
 };
